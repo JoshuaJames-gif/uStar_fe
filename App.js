2e84305aefe8005d9743fffde09074e3bfa50fc5
Joshua Simpson <simpsonjosh16@gmail.com>
2020-09-15 10:20:46 +0100

    Made App.js into Class

--- App.js
+++ App.js
@@ -1,34 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Header from './components/header'
-import Login from './components/login'
-import fire from './fire'
-import * as firebase from 'firebase'
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Header from "./components/header";
+import Login from "./components/login";
+import Fire from "./fire";
+import * as firebase from "firebase";
 
-export default function App() {
-  return (
+export default class App extends React.Component {
+  state = {};
+
+  render() {
     <View style={styles.container}>
       <Header />
       <View style={styles.content}>
         <text>Please Sign in</text>
         <View style={styles.list}>
           <Login />
-          <View styles={styles.button}>
-          {/* {signup button} */}
-          </View>
+          <View styles={styles.button}>{/* {signup button} */}</View>
         </View>
       </View>
       <StatusBar style="auto" />
-    </View>
-  );
+    </View>;
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- fire.js
+++ fire.js
@@ -1,5 +1,5 @@
-export default {
-  firebaseConfig: {
+const config = {
+  
     apiKey: "AIzaSyDjAi4nnoGRjD4sztWF9nPHBduOy7axSVM",
     authDomain: "ustar-project.firebaseapp.com",
     databaseURL: "https://ustar-project.firebaseio.com",
@@ -7,5 +7,7 @@ export default {
     storageBucket: "ustar-project.appspot.com",
     messagingSenderId: "202087391894",
     appId: "1:202087391894:web:9881624e18497a52bb18b7",
-  },
+
 };
+const Fire = firebase.initializeApp(config)
+export default Fire;
