f1c700551a7cc7b26fa7f9480367f2b84d397999
Joshua Simpson <simpsonjosh16@gmail.com>
2020-09-17 10:14:42 +0100

    revised app, screens folder, navigation folder

--- App.js
+++ App.js
@@ -1,52 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import "firebase/auth";
-import Header from "./components/header";
-import Login from "./components/login";
-import Fire from "./fire";
-import * as firebase from "firebase";
-import Home from "./components/home";
-import Signup from "./components/Signup";
-
-// firebase.initializeApp(config);
-// const Fire = firebase.initializeApp(config);
+import SwitchNavigator from "../";
 
 export default class App extends React.Component {
-  state = {
-    user: {},
-    // isLoading: false,
-    // isAuthenticationReady: false,
-    // isAuthenticated: false,
-  };
-
-  authListener = () => {
-    Fire.auth().onAuthStateChanged((user) => {
-      if (user) {
-        this.setState({ user });
-      } else {
-        this.setState({ user: null });
-      }
-    });
-  };
-  componentDidMount = () => {
-    this.authListener();
-  };
-
   render() {
-    return (
-      <View style={styles.container}>
-        <Header />
-        <View style={styles.content}>
-          {/* <Text>Please Sign in</Text>
-          <View style={styles.list}>
-            {this.state.user.email ? <Home /> : <Login />}
-            <View styles={styles.button}>{/* {signup button} */}
-          <Signup />
-        </View>
-        <StatusBar style="auto" />
-      </View>
-    );
+    return <SwitchNavigator />;
   }
 }
 
---
+++ screens/Profile.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+import Firebase from "../config/Firebase";
+
+class Profile extends React.Component {
+  handleSignout = () => {
+    Firebase.auth().signOut();
+    this.props.navigation.navigate("Login");
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Profile Screen</Text>
+        <Text>{this.props.email}</Text>
+        <Button title="Logout" onPress={this.handleSignout} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+export default Profile;
