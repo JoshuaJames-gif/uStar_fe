c94d3c38307fc36623c64eac509569bbf40c5f8c
Shalommoise <mshalom689@gmail.com>
2020-09-21 16:54:21 +0100

    created task list by child

--- screens/ChildPage.js
+++ screens/ChildPage.js
@@ -12,49 +12,66 @@ import * as api from "../utils/api";
 
 class ChildPage extends Component {
   state = {
-    child: { child_id: 1, child_name: "Charles", star_count: 5 },
-    tasks: [
-      { task_description: "wash clothes", stars_worth: 10, task_id: 98 },
-      { task_description: "wash car", stars_worth: 20, task_id: 898 },
-    ],
+    isParentLoggedIn: false,
+    child: { child_id: 5, child_name: "James", star_count: 12 },
+    tasks: [],
   };
 
-  list = () => {
-    return this.state.tasks.map((task) => {
-      return (
-        <View key={task.task_id}>
-          <Text>{task.task_description}</Text>
-          <Text>{task.stars_worth}</Text>
-        </View>
-      );
+  getTasks = (child_id) => {
+    api.fetchTasksByChild(child_id).then((tasks) => {
+      this.setState({ tasks });
     });
   };
-
+  handleReviewPushByChild = (event) => {
+    const task_id = event.target.id;
+    api.updateTaskByChild(task_id).then(() => {
+      this.getTasks(this.state.child.child_id);
+    });
+  };
+  componentDidMount = () => {
+    this.getTasks(this.state.child.child_id);
+  };
+  componentDidUpdate = (prevProps, prevState) => {
+    if (prevState.tasks.length !== this.state.tasks.length)
+      this.getTasks(this.state.child.child_id);
+  };
   render() {
     const { child_name, star_count } = this.state.child;
     return (
       <View style={styles.container}>
         <Text>
-          {child_name}:{star_count}
+          Hi {child_name}, you currently have {star_count} stars.
         </Text>
-        <View>{this.list()}</View>
+        <View style={styles.list}>
+          {this.state.tasks.map((task) => {
+            return (
+              <View key={task.task_id} style={styles.listItem}>
+                <Text>The task is to {task.task_description}</Text>
+                <Text>Is worth {task.stars_worth} ⭐</Text>
+                <Text>Status: {task.task_status}</Text>
+                <TouchableOpacity
+                  onPress={this.handleReviewPushByChild}
+                  style={styles.button}
+                >
+                  <Text id={task.task_id} style={styles.buttonText}>
+                    Request Review{" "}
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            );
+          })}
+        </View>
       </View>
     );
   }
 }
-componentDidMount = () => {
-  getChildByParent = () => {
-    api.fetchTasksByChild().then((res) => {
-      this.setState({ tasks: res });
-    });
-  };
-};
+
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "#ffff",
     alignItems: "center",
     justifyContent: "center",
-    flex: 0.75,
+    flex: 1,
     height: 300,
     width: 500,
     overflow: "hidden",
@@ -63,6 +80,31 @@ const styles = StyleSheet.create({
     borderTopRightRadius: 20,
     borderBottomLeftRadius: 20,
     borderBottomRightRadius: 20,
+  },
+  listItem: {
+    padding: 10,
+    borderTopWidth: 5,
+    borderBottomWidth: 5,
+    borderColor: "black",
+  },
+  list: {
+    margin: 20,
+  },
+  button: {
+    marginTop: 30,
+    marginBottom: 20,
+    paddingVertical: 5,
+    alignItems: "center",
+    backgroundColor: "#E76F51",
+    borderColor: "#E76F51",
+    borderWidth: 1,
+    borderRadius: 5,
+    width: 200,
+  },
+  buttonText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#fff",
   },
 });
 export default ChildPage;
--- utils/api.js
+++ utils/api.js
@@ -1,9 +1,19 @@
 import axios from "axios";
 //https://ustar-backend.herokuapp.com/api/children/3/tasks/
-export const fetchTasksByChild = () => {
+export const fetchTasksByChild = (child_id) => {
   return axios
-    .get("https://ustar-backend.herokuapp.com/api/children/3/tasks/")
+    .get(`https://ustar-backend.herokuapp.com/api/children/${child_id}/tasks/`)
     .then((res) => {
-      return res;
+      return res.data.tasks;
+    });
+};
+
+export const updateTaskByChild = (task_id) => {
+  return axios
+    .patch(`https://ustar-backend.herokuapp.com/api/tasks/${task_id}`, {
+      task_status: "pending",
+    })
+    .then((res) => {
+      return res.data.task;
     });
 };
