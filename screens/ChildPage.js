268206ec5a404fc7631b31480c36ef20a4a9caca
Aadil Mohamed <you@example.com>
2020-09-22 16:57:55 +0100

    refactored updateChild to be more dynamic and allow parent to accept, removeChild, createChild

--- screens/ChildPage.js
+++ screens/ChildPage.js
@@ -7,14 +7,17 @@ import {
   TouchableOpacity,
   Text,
   Button,
+  ScrollView,
 } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
 import * as api from "../utils/api";
 
 class ChildPage extends Component {
   state = {
-    isParentLoggedIn: false,
-    child: { child_id: 2, child_name: "James", star_count: 37 },
+    isParentLoggedIn: true,
+    child: { child_id: 7, child_name: "James", star_count: 37 },
     tasks: [],
+    newTask: { task_description: "", stars_worth: "" },
   };
 
   getTasks = (child_id) => {
@@ -22,52 +25,153 @@ class ChildPage extends Component {
       this.setState({ tasks });
     });
   };
-  handleReviewPushByChild = (task_id) => {
-    api.updateTaskByChild(task_id).then(() => {
+  handleReviewPushByChild = (task_id, task_status) => {
+    api.updateTaskByChild(task_id, task_status).then(() => {
+      this.getTasks(child.child_id);
+    });
+  };
+  handlePost = (child_id, task_description, stars_worth) => {
+    api.createTask(child_id, task_description, stars_worth).then(() => {
       this.getTasks(this.state.child.child_id);
+      this.setState({ newTask: { task_description: "", stars_worth: "" } });
+    });
+  };
+  handleDeleteTask = (task_id) => {
+    api.removeTask(task_id).then(() => {
+      this.setState((currentState) => {
+        return {
+          tasks: currentState.tasks.filter((task) => task.task_id !== task_id),
+        };
+      });
     });
   };
   componentDidMount = () => {
     this.getTasks(this.state.child.child_id);
   };
   componentDidUpdate = (prevProps, prevState) => {
-    if (prevState.tasks.length !== this.state.tasks.length)
+    if (prevState.tasks.length !== this.state.tasks.length) {
       this.getTasks(this.state.child.child_id);
+    }
   };
   render() {
-    const { child_name, star_count } = this.state.child;
+    const { child_name, star_count, child_id } = this.state.child;
+    const { isParentLoggedIn, tasks, child, newTask } = this.state;
+
     return (
       <View style={styles.container}>
         <Text>
           Hi {child_name}, you currently have {star_count} stars.
         </Text>
-        <View style={styles.list}>
-          {this.state.tasks.map((task) => {
-            return (
-              <View key={task.task_id} style={styles.listItem}>
-                <Text>The task is to {task.task_description}</Text>
-                <Text>Is worth {task.stars_worth} ⭐</Text>
-                <Text>Status: {task.task_status}</Text>
-                <TouchableOpacity
-                  id={task.task_id}
-                  key={task.task_id}
-                  onPress={() => {
-                    this.handleReviewPushByChild(task.task_id);
-                  }}
-                  style={styles.button}
-                >
-                  <Text
-                    id={task.task_id}
-                    key={task.task_id}
-                    style={styles.buttonText}
-                  >
-                    Request Review{" "}
-                  </Text>
-                </TouchableOpacity>
-              </View>
-            );
-          })}
-        </View>
+        <ScrollView>
+          <View style={styles.list}>
+            {tasks.map((task) => {
+              const {
+                task_status,
+                task_description,
+                stars_worth,
+                task_id,
+              } = task;
+
+              return (
+                <View key={task_id} style={styles.listItem}>
+                  <Text>The task is to {task_description}</Text>
+                  <Text>Is worth {stars_worth} ⭐</Text>
+                  <Text>Status: {task_status}</Text>
+                  {isParentLoggedIn ? (
+                    <View>
+                      <TouchableOpacity
+                        onPress={() =>
+                          this.handleReviewPushByChild(task_id, "completed")
+                        }
+                        style={
+                          task_status === "completed"
+                            ? styles.disabledButton
+                            : styles.button
+                        }
+                        disabled={task_status === "completed"}
+                      >
+                        <Text id={task_id} style={styles.buttonText}>
+                          Confirm request
+                        </Text>
+                      </TouchableOpacity>
+                      <TouchableOpacity
+                        onPress={() => this.handleDeleteTask(task_id)}
+                        style={styles.button}
+                      >
+                        <Text style={styles.buttonText}>Delete </Text>
+                      </TouchableOpacity>
+                    </View>
+                  ) : (
+                    <TouchableOpacity
+                      style={
+                        task_status === "pending"
+                          ? styles.disabledButton
+                          : styles.button
+                      }
+                      disabled={task_status === "pending"}
+                      onPress={() => {
+                        this.handleReviewPushByChild(task_id, "pending");
+                      }}
+                    >
+                      <Text
+                        id={task.task_id}
+                        key={task.task_id}
+                        style={styles.buttonText}
+                      >
+                        Request Review
+                      </Text>
+                    </TouchableOpacity>
+                  )}
+                </View>
+              );
+            })}
+          </View>
+          {isParentLoggedIn && (
+            <View style={styles.container}>
+              <TextInput
+                style={styles.inputBox}
+                value={newTask.task_description}
+                onChangeText={(task_description) =>
+                  this.setState((currentState) => {
+                    return {
+                      newTask: { ...newTask, task_description },
+                    };
+                  })
+                }
+                placeholder="New task description"
+              />
+              <TextInput
+                style={styles.inputBox}
+                value={newTask.stars_worth}
+                onChangeText={(stars_worth) =>
+                  this.setState((currentState) => {
+                    return {
+                      newTask: { ...newTask, stars_worth },
+                    };
+                  })
+                }
+                placeholder="Stars worth"
+              />
+              <TouchableOpacity
+                style={
+                  newTask.task_description === ""
+                    ? styles.disabledButton
+                    : styles.button
+                }
+                disabled={newTask.task_description === ""}
+                onPress={() => {
+                  this.handlePost(
+                    child_id,
+                    newTask.task_description,
+                    newTask.stars_worth
+                  );
+                }}
+              >
+                <Text style={styles.buttonText}>Add new task</Text>
+              </TouchableOpacity>
+            </View>
+          )}
+        </ScrollView>
       </View>
     );
   }
@@ -96,6 +200,8 @@ const styles = StyleSheet.create({
   },
   list: {
     margin: 20,
+    justifyContent: "center",
+    alignItems: "center",
   },
   button: {
     marginTop: 30,
@@ -108,10 +214,30 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     width: 200,
   },
+  disabledButton: {
+    marginTop: 30,
+    marginBottom: 20,
+    paddingVertical: 5,
+    alignItems: "center",
+    backgroundColor: "gray",
+    borderColor: "gray",
+    borderWidth: 1,
+    borderRadius: 5,
+    width: 200,
+  },
   buttonText: {
     fontSize: 20,
     fontWeight: "bold",
     color: "#fff",
+  },
+  inputBox: {
+    width: "85%",
+    margin: 10,
+    padding: 15,
+    fontSize: 16,
+    borderColor: "#d3d3d3",
+    borderBottomWidth: 1,
+    textAlign: "center",
   },
 });
 export default ChildPage;
--- utils/api.js
+++ utils/api.js
@@ -8,13 +8,23 @@ export const fetchTasksByChild = (child_id) => {
     });
 };
 
-export const updateTaskByChild = (task_id) => {
-  console.log(task_id);
+export const updateTaskByChild = (task_id, task_status) => {
   return axios
     .patch(`https://ustar-backend.herokuapp.com/api/tasks/${task_id}`, {
-      task_status: "pending",
+      task_status,
     })
     .then((res) => {
       return res.data.task;
     });
+};
+export const removeTask = (task_id) => {
+  return axios.delete(
+    `https://ustar-backend.herokuapp.com/api/tasks/${task_id}`
+  );
+};
+export const createTask = (child_id, task_description, stars_worth) => {
+  return axios.post(
+    `https://ustar-backend.herokuapp.com/api/children/${child_id}/tasks`,
+    { task_description, stars_worth }
+  );
 };
